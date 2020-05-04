6bac0b33e0a0933d654930e8144090ee427914ad
dav1dyang <dy1096@nyu.edu>
2020-05-03 21:13:52 -0700

    May 3rd 2020 Update
    Set up cretificite for the server,  use https, create google login button, and rename sign in sign out sign up.

--- Server/server.js
+++ Server/server.js
@@ -4,15 +4,18 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const PORT = 4444;
+const HTTPSPORT1 = 443;
+const HTTPSPORT2 = 444;
+const HTTPPORT1 = 80;
 require('dotenv').config();
 
-//----------cookie, session, and user login middleware----------//
+//----------cookie, session, and user signin middleware----------//
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const nedbstore = require('nedb-session-store')(session);
 const bcrypt = require('bcrypt');
 const uuidV1 = require('uuid/v1'); //running with uuid@3 version in order to work
+
 
 //----------text analysis middleware----------//
 const Datastore = require('nedb'); //future update to mongoDB
@@ -21,28 +24,42 @@ const Mercury = require('@postlight/mercury-parser');
 const multer = require('multer');
 //const language = require('@google-cloud/language');
 
-//----------global variable setup----------//
-var credentials = {
-	key: fs.readFileSync('star_itp_io.key'),
-	cert: fs.readFileSync('star_itp_io.pem')
-};
+//----------Google Sign In option----------//
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+async function verify() {
+	const ticket = await client.verifyIdToken({
+		idToken: token,
+		audience: '382219507043-mn78i1knjl39o7ghstgc04aint0hqkb4.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+		// Or, if multiple clients access the backend:
+		//[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+	});
+	const payload = ticket.getPayload();
+	const userid = payload['sub'];
+	// If request specified a G Suite domain:
+	//const domain = payload['hd'];
+}
+verify().catch(console.error);
 
+//----------global variable setup----------//
+var DYCredentials = {
+	key: fs.readFileSync('DYprivkey.key'),
+	cert: fs.readFileSync('DYfullchain.pem')
+};
+// var AtmosCredentials = {
+// 	key: fs.readFileSync('Atmosprivkey.key'),
+// 	cert: fs.readFileSync('Atmosfullchain.pem')
+// };
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: true }); // for parsing form data
 var usersdataDB = new Datastore({ filename: "usersdata.db", autoload: true });
 var registerDB = new Datastore({ filename: "registration.db", autoload: true });
-
-
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(cors());
 app.use(urlencodedParser);
 app.use(cookieParser()); // Use the cookieParser middleware
-
-
-
-//----------cookie, session, and Login----------//
 app.use(
 	session(
 		{
@@ -57,6 +74,7 @@ app.use(
 	)
 );
 
+//----------cookie, session, and sign in----------//
 app.get('/', (req, res) => {
 
 	if (req.session.username) {
@@ -82,22 +100,22 @@ app.get('/', (req, res) => {
 		// Send the info to the user
 		res.send('You have visited this site ' + visits + ' times.' + 'session user-id: ' + req.session.userid + '. ');
 	} else {
-		delete req.session.loginStatus;
-		res.redirect('/login');
+		delete req.session.signInStatus;
+		res.render('homepageSignIn.ejs', {});
 	}
 });
 
-//storing registration data to database
+//storing sign up data to database
 app.post('/register', function (req, res) {
 	registerDB.findOne({ 'username': req.body.username }, (err, doc) => {
 		if (doc != null) {
 			req.session.alreadyRegister = 'Username already being used!';
-			res.redirect('/registration');
+			res.redirect('/signup');
 		} else {
 			delete req.session.alreadyRegister;
 
 			var passwordHash = generateHash(req.body.password); // We want to 'hash' the password so that it isn't stored in clear text in the database
-			console.log(`entered: ${req.body.password} and stored ${passwordHash}`);
+			//console.log(`entered: ${req.body.password} and stored ${passwordHash}`);
 
 			// The information we want to store
 			var registration = {
@@ -106,62 +124,65 @@ app.post('/register', function (req, res) {
 			};
 
 			registerDB.insert(registration); // Insert into the database
-			console.log('inserted ' + registration);
+			//console.log('inserted ' + registration);
 
-			res.send('Registered <a href="dy1096.itp.io:3000/">sign in</a>'); // Give the user an option of what to do next
+			req.session.signInStatus = `Hi ${req.body.username}! Please sign in:`;
+			res.redirect('/signin');
+			//res.send('<html><head></head><body><h1>Registered</h1><a href="https://dy1096.itp.io:443/">sign in</a></body></html>'); // Give the user an option of what to do next
 		}
 	});
 });
 
-app.post('/loginsubmit', (req, res) => {
+//check-in if the sign in info match the register database
+app.post('/signinsubmit', (req, res) => {
 	registerDB.findOne({ 'username': req.body.username }, (err, doc) => {
 		if (doc != null) {
 			console.log('password + hash: ' + req.body.username + ' ' + req.body.password + ' ' + doc.password);
 			if (compareHash(req.body.password, doc.password)) {
-				console.log('logincomparedtrue');
-				req.session.loginStatus = 'Loggedin';
+				console.log('signincomparedtrue');
+				req.session.signInStatus = 'Signned in';
 				req.session.username = doc.username;
-				req.session.lastlogin = Date.now();
+				req.session.lastSignIn = Date.now();
 				res.redirect('/');
 			} else {
-				console.log('login Invalid password');
-				req.session.loginStatus = 'INVALID: renter your password';
-				res.redirect('/login');
+				console.log('signin Invalid password');
+				req.session.signInStatus = 'INVALID: renter your password';
+				res.redirect('/signin');
 			}
 		} else {
 			if (req.body.username != '') {
-				console.log('login Invalid no match username');
-				req.session.loginStatus = 'INVALID: Please try again';
-				res.redirect('/login');
+				console.log('signin Invalid no match username');
+				req.session.signInStatus = 'INVALID: Please try again';
+				res.redirect('/signin');
 			} else {
-				console.log('login Invalid no entry');
-				req.session.loginStatus = 'INVALID: Please enter your username and password';
-				res.redirect('/login');
+				console.log('signin Invalid no entry');
+				req.session.signInStatus = 'INVALID: Please enter your username and password';
+				res.redirect('/signin');
 			}
 		}
 	});
 })
 
-//Link to the login page
-app.get('/login', (req, res) => {
+//Link to the sign in page
+app.get('/signin', (req, res) => {
 	delete req.session.alreadyRegister;
-	var data = { status: req.session.loginStatus }
-	//console.log(req.session.loginStatus);
-	res.render('login.ejs', { data });
+	var data = { status: req.session.signInStatus }
+	//console.log(req.session.signInStatus);
+	res.render('signIn.ejs', { data });
 });
 
 // Link to the registration page
-app.get('/registration', (req, res) => {
+app.get('/signup', (req, res) => {
 	var data = { register: req.session.alreadyRegister };
 	//console.log(req.session.alreadyRegister);
-	res.render('registration.ejs', { data });
+	res.render('signUp.ejs', { data });
 });
 
-// Link to the logout
-app.get('/logout', (req, res) => {
+// Link to the signout
+app.get('/signout', (req, res) => {
 	delete req.session.alreadyRegister;
 	delete req.session.username;
-	console.log('loggedout');
+	console.log('signed out');
 	res.redirect('/');
 });
 
@@ -176,6 +197,18 @@ function compareHash(password, hash) {
 	return bcrypt.compareSync(password, hash);
 }
 
-//----------open port----------// 
-var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(PORT); 
+//----------open https port----------// 
+var DYHttpsServer = https.createServer(DYCredentials, app);
+DYHttpsServer.listen(HTTPSPORT1);
+
+// var AtmosHttpsServer = https.createServer(AtmosCredentials, app);
+// AtmosHttpsServer.listen(HTTPSPORT2);
+
+//----------http connection----------// 
+var httpapp = express();
+httpapp.use(express.static('public'));
+httpapp.get('/', (req, res) => {
+	res.send('<html><head></head><body><h1>http</h1><a href="https://davidyang.cc/">To https</a></body></html>');
+});
+
+httpapp.listen(HTTPPORT1);
