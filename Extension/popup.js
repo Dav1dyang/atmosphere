97017806a1c140c4ceeb185cfd0ef2a6698eff05
dav1dyang <dy1096@nyu.edu>
2020-05-06 09:18:46 -0700

    Sign in system

---
+++ Extension/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', clickHandler);
+    main();
+});
+
+function main() {
+    // location.href = 'https://davidyang.cc';
+}
--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 //----------server essentials setup----------//
 const https = require('https');
 const bodyParser = require('body-parser');
@@ -7,7 +9,7 @@ const fs = require('fs');
 const HTTPSPORT1 = 443;
 const HTTPSPORT2 = 444;
 const HTTPPORT1 = 80;
-require('dotenv').config();
+const fetch = require("node-fetch");
 
 //----------cookie, session, and user signin middleware----------//
 const cookieParser = require('cookie-parser');
@@ -26,8 +28,9 @@ const multer = require('multer');
 
 //----------Google Sign In option----------//
 const { OAuth2Client } = require('google-auth-library');
-const client = new OAuth2Client(CLIENT_ID);
-async function verify() {
+const client = new OAuth2Client('382219507043-mn78i1knjl39o7ghstgc04aint0hqkb4.apps.googleusercontent.com');
+
+async function verify(token) {
 	const ticket = await client.verifyIdToken({
 		idToken: token,
 		audience: '382219507043-mn78i1knjl39o7ghstgc04aint0hqkb4.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
@@ -37,9 +40,8 @@ async function verify() {
 	const payload = ticket.getPayload();
 	const userid = payload['sub'];
 	// If request specified a G Suite domain:
-	//const domain = payload['hd'];
+	//const domain = payload['hd'];	
 }
-verify().catch(console.error);
 
 //----------global variable setup----------//
 var DYCredentials = {
@@ -69,15 +71,18 @@ app.use(
 			},
 			store: new nedbstore({
 				filename: 'sessions.db'
-			})
+			}),
+			resave: true,
+			secure: true,
+			saveUninitialized: true
 		}
 	)
 );
 
 //----------cookie, session, and sign in----------//
 app.get('/', (req, res) => {
-
-	if (req.session.username) {
+	//console.log(req.session.username);
+	if (req.session.username != null) {
 
 		if (!req.session.userid) {
 			req.session.userid = uuidV1();
@@ -98,10 +103,25 @@ app.get('/', (req, res) => {
 		res.cookie('visits', visits, {});
 
 		// Send the info to the user
-		res.send('You have visited this site ' + visits + ' times.' + 'session user-id: ' + req.session.userid + '. ');
+		let data = {
+			visits: visits,
+			session_id: req.session.userid,
+			signedIn: true
+		}
+		res.render('homepage.ejs', { data });
+		//res.send('You have visited this site ' + visits + ' times.' + 'session user-id: ' + req.session.userid + '. ');
 	} else {
-		delete req.session.signInStatus;
-		res.render('homepageSignIn.ejs', {});
+		if (!req.session.userid) {
+			req.session.userid = uuidV1();
+		}
+		req.session.signInStatus = null;
+
+		let data = {
+			visits: visits,
+			session_id: req.session.userid,
+			signedIn: false
+		}
+		res.render('homepage.ejs', { data });
 	}
 });
 
@@ -112,36 +132,41 @@ app.post('/register', function (req, res) {
 			req.session.alreadyRegister = 'Username already being used!';
 			res.redirect('/signup');
 		} else {
-			delete req.session.alreadyRegister;
+			req.session.alreadyRegister = null;
 
 			var passwordHash = generateHash(req.body.password); // We want to 'hash' the password so that it isn't stored in clear text in the database
+
 			//console.log(`entered: ${req.body.password} and stored ${passwordHash}`);
 
 			// The information we want to store
-			var registration = {
+
+			let registration = {
 				'username': req.body.username,
+				'firstname': req.body.firstname,
+				'lastname': req.body.lastname,
 				'password': passwordHash
 			};
-
+			req.session.signInName = req.body.firstname;
 			registerDB.insert(registration); // Insert into the database
 			//console.log('inserted ' + registration);
-
-			req.session.signInStatus = `Hi ${req.body.username}! Please sign in:`;
+			req.session.signedIn = false;
+			req.session.signInStatus = null;
 			res.redirect('/signin');
 			//res.send('<html><head></head><body><h1>Registered</h1><a href="https://dy1096.itp.io:443/">sign in</a></body></html>'); // Give the user an option of what to do next
 		}
 	});
+	req.session.save();
 });
 
 //check-in if the sign in info match the register database
 app.post('/signinsubmit', (req, res) => {
 	registerDB.findOne({ 'username': req.body.username }, (err, doc) => {
 		if (doc != null) {
-			console.log('password + hash: ' + req.body.username + ' ' + req.body.password + ' ' + doc.password);
-			if (compareHash(req.body.password, doc.password)) {
+			if (compareHash(req.body.password, doc.password) && doc.password != null) {
 				console.log('signincomparedtrue');
-				req.session.signInStatus = 'Signned in';
-				req.session.username = doc.username;
+				req.session.signInStatus = 'signned in';
+				req.session.signedIn = true;
+				req.session.username = doc.firstname;
 				req.session.lastSignIn = Date.now();
 				res.redirect('/');
 			} else {
@@ -152,38 +177,114 @@ app.post('/signinsubmit', (req, res) => {
 		} else {
 			if (req.body.username != '') {
 				console.log('signin Invalid no match username');
-				req.session.signInStatus = 'INVALID: Please try again';
+				req.session.signInStatus = 'INVALID: no matched username';
 				res.redirect('/signin');
 			} else {
 				console.log('signin Invalid no entry');
-				req.session.signInStatus = 'INVALID: Please enter your username and password';
+				req.session.signInStatus = 'INVALID: please enter your username and password';
 				res.redirect('/signin');
 			}
 		}
 	});
+	req.session.save();
 })
 
 //Link to the sign in page
 app.get('/signin', (req, res) => {
-	delete req.session.alreadyRegister;
-	var data = { status: req.session.signInStatus }
-	//console.log(req.session.signInStatus);
-	res.render('signIn.ejs', { data });
+	//console.log(req.session.signedIn);
+	if (req.session.signedIn == true) {
+		res.render('alreadySignedIn.ejs', {});
+	} else {
+		if (req.query.new || req.query.submit == '') {
+			req.session.alreadyRegister = null;
+			req.session.signInStatus = null;
+			var data = { status: req.session.signInStatus, name: null }
+			//console.log(req.session.signInStatus);
+			res.render('signIn.ejs', { data });
+		} else {
+			req.session.alreadyRegister = null;
+
+			var data = { status: req.session.signInStatus, name: req.session.signInName }
+			//console.log(req.session.signInStatus);
+			res.render('signIn.ejs', { data });
+		}
+	}
 });
 
 // Link to the registration page
 app.get('/signup', (req, res) => {
-	var data = { register: req.session.alreadyRegister };
-	//console.log(req.session.alreadyRegister);
-	res.render('signUp.ejs', { data });
+	if (req.query.new || req.query.submit == '') {
+		req.session.alreadyRegister = null;
+		var data = { register: req.session.alreadyRegister };
+		res.render('signUp.ejs', { data });
+	} else {
+		var data = { register: req.session.alreadyRegister };
+		//console.log(req.session.alreadyRegister);
+		res.render('signUp.ejs', { data });
+	}
 });
 
 // Link to the signout
 app.get('/signout', (req, res) => {
-	delete req.session.alreadyRegister;
-	delete req.session.username;
+	req.session.signedIn = false;
+	req.session.alreadyRegister = null;
+	req.session.username = null;
+	req.session.signInName = null;
 	console.log('signed out');
 	res.redirect('/');
+});
+
+app.post('/signoutgoogle', (req, res) => {
+	if (req.body.signoutgoogle && req.body.signoutgoogle != null) {
+		req.session.signedIn = false;
+		req.session.alreadyRegister = null;
+		req.session.username = null;
+		req.session.signInName = null;
+		// console.log(req.session.signedIn);
+		console.log('google signed out');
+	} else {
+		console.log('google still signed in');
+	}
+	req.session.save();
+});
+
+app.post('/tokensignin', (req, res) => {
+	// console.log('token: ' + req.body.idtoken);
+	req.session.signedIn = true;
+	req.session.lastSignIn = Date.now();
+	req.session.save();
+	var googleUser;
+	fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${req.body.idtoken}`)
+		.then(res => res.json())
+		.then(data => googleUser = data)
+		.then(() => {
+			// console.log('all: ' + googleUser);
+			// console.log('name: ' + googleUser.name);
+			registerDB.findOne({ 'username': googleUser.sub }, (err, doc) => {
+				if (doc != null) {
+					// req.session.googleRegistered = true;
+					req.session.username = doc.firstname;
+					console.log('google user signed in');
+					// console.log(doc.firstname);
+					// console.log(req.session.username);
+					// console.log(req.session.signedIn);
+					req.session.save();
+				} else {
+					req.session.alreadyRegister = null;
+					req.session.username = googleUser.given_name;
+					let registration = {
+						'username': googleUser.sub,
+						'firstname': googleUser.given_name,
+						'lastname': googleUser.family_name,
+						'password': null
+					};
+					registerDB.insert(registration);
+					console.log('registered new google user into database');
+					req.session.save();
+				}
+			});
+
+		});
 });
 
 // generate password's hash
@@ -199,7 +300,9 @@ function compareHash(password, hash) {
 
 //----------open https port----------// 
 var DYHttpsServer = https.createServer(DYCredentials, app);
-DYHttpsServer.listen(HTTPSPORT1);
+DYHttpsServer.listen(HTTPSPORT1, () => {
+	console.log(` HTTPS listening at port ${HTTPSPORT1}`)
+});
 
 // var AtmosHttpsServer = https.createServer(AtmosCredentials, app);
 // AtmosHttpsServer.listen(HTTPSPORT2);
@@ -210,5 +313,6 @@ httpapp.use(express.static('public'));
 httpapp.get('/', (req, res) => {
 	res.send('<html><head></head><body><h1>http</h1><a href="https://davidyang.cc/">To https</a></body></html>');
 });
-
-httpapp.listen(HTTPPORT1);
+httpapp.listen(HTTPPORT1, () => {
+	console.log(` HTTP listening at port ${HTTPPORT1}`)
+});
